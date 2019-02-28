18bcc427205526b23b01cfacd68fd977342cc11d
AC De Leon <aadeleon@sdsu.edu>
2019-02-27 17:55:32 -0800

    [+] react-player

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactPlayer from "react-player";
 import astronaut from "./astronaut.svg";
 import "./App.css";
 
@@ -6,15 +7,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="player-wrapper">
+          <ReactPlayer
+            className="react-player"
+            url="https://soundcloud.com/not-bees/datewave"
+            playing
+            loop
+          />
+        </div>
         <div className="Astronaut">
           <img src={astronaut} className="App-logo" alt="logo" />
         </div>
         <div className="Textbox">
           <p>
-            <font color="white">You are not alone.</font>
+            <font color="#D1BEE5">You are not alone.</font>
           </p>
           <p>
-            <font color="white">I'm here for you.</font>
+            <font color="#D1BEE5">I'm here for you.</font>
           </p>
         </div>
       </div>
@@ -22,4 +31,5 @@ class App extends Component {
   }
 }
 
+// https://facebook.github.io/create-react-app/docs/adding-images-fonts-and-files
 export default App;
