91e38c3cfe2573731607ff4401b8fde4840dfcfc
AC De Leon <aadeleon@sdsu.edu>
2019-03-02 13:59:53 -0800

    [+] Testing react-spotify-player credentials

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from "react";
-import SoundCloudPlayer from "react-player/lib/players/SoundCloud";
-import astronaut from "./astronaut.svg";
+import ReactPlayer from "react-player";
+import SpotifyPlayer from "react-spotify-player";
+import astronaut from "./images/astronaut.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
+    // size may also be a plain string using the presets 'large' or 'compact'
+    const size = {
+      width: "100%",
+      height: 300,
+    };
+    const view = "list"; // or 'coverart'
+    const theme = "black"; // or 'white'
     return (
       <div className="App">
-        <SoundCloudPlayer
+        {/* <ReactPlayer
           className="react-player"
-          url="https://soundcloud.com/not-bees/datewave"
           playing="true"
-          loop="true"
-        />
+          url={[
+            "https://soundcloud.com/not-bees/datewave",
+          ]}
+        /> */}
         <div className="Astronaut">
           <img src={astronaut} className="App-logo" alt="acfromspace" />
         </div>
         <div className="Textbox">
+          <SpotifyPlayer
+            uri="spotify:user:1252712964:playlist:0XVxReRFFUe7Z5DFYPdOSU"
+            size={size}
+            view={view}
+            theme={theme}
+          />
           <h1>YOU ARE LOVED</h1>
         </div>
       </div>
@@ -24,5 +39,4 @@ class App extends Component {
   }
 }
 
-// https://facebook.github.io/create-react-app/docs/adding-images-fonts-and-files
 export default App;
