37814c808a41d8caac29e7e06c215cda14eef4be
AC De Leon <aadeleon@sdsu.edu>
2019-03-02 16:43:54 -0800

    [+] working layout, needs cleaning via variables and components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import ReactPlayer from "react-player";
+// import ReactPlayer from "react-player";
 import SpotifyPlayer from "react-spotify-player";
 import astronaut from "./images/astronaut.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
-    // size may also be a plain string using the presets 'large' or 'compact'
+    const uniqueTitle = "unique";
+    const width = "300";
+    const height = "500";
     const size = {
       width: 300,
       height: 500,
@@ -19,20 +21,32 @@ class App extends Component {
           <img src={astronaut} className="App-logo" alt="acfromspace" />
         </div>
         <div className="Players">
-          <ReactPlayer
-            url={["https://soundcloud.com/acfromspace/sets/roxanne"]}
-            width="300px"
-            height="500px"
+          <iframe
+            title={uniqueTitle}
+            width={width}
+            height={height}
+            src="https://www.youtube.com/embed/dOFsCX72jyU"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
           />
-          <ReactPlayer
-            url={["https://soundcloud.com/acfromspace/sets/roxanne"]}
-            width="300px"
-            height="500px"
+          <iframe
+            title={uniqueTitle}
+            width={width}
+            height={height}
+            scrolling="no"
+            frameborder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/204382042&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
           />
-          <ReactPlayer
-            url={["https://soundcloud.com/acfromspace/sets/roxanne"]}
-            width="300px"
-            height="500px"
+          <iframe
+            title={uniqueTitle}
+            src="https://open.spotify.com/embed/user/1252712964/playlist/1su6mhUPJxkVvwhmBAwpPj"
+            width={width}
+            height={height}
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
           />
           <SpotifyPlayer
             uri="spotify:user:1252712964:playlist:0XVxReRFFUe7Z5DFYPdOSU"
@@ -41,20 +55,14 @@ class App extends Component {
             theme={theme}
           />
           <SpotifyPlayer
-            uri="spotify:user:1252712964:playlist:0XVxReRFFUe7Z5DFYPdOSU"
-            size={size}
-            view={view}
-            theme={theme}
-          />
-          <SpotifyPlayer
-            uri="spotify:user:1252712964:playlist:0XVxReRFFUe7Z5DFYPdOSU"
+            uri="spotify:user:spotify:playlist:37i9dQZF1DWTTHy0vndijk"
             size={size}
             view={view}
             theme={theme}
           />
         </div>
         <div className="Footer">
-          <h1>YOU ARE LOVED</h1>
+          <h1>"YOU ARE FILLED WITH DETERMINATION"</h1>
         </div>
       </div>
     );
