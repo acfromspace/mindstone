af1dd0e707b443e956a46cacfaf74a992bfdf83f
AC De Leon <aadeleon@sdsu.edu>
2019-03-04 16:29:40 -0800

    [*] modularized components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../App.css";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <h1>"YOU ARE FILLED WITH DETERMINATION"</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/Players.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import "../App.css";
+
+export default class Players extends Component {
+  render() {
+    const uniqueTitle = "unique";
+    const width = "300";
+    const height = "500";
+    return (
+      <div className="Players">
+        <iframe
+          title={uniqueTitle}
+          width={width}
+          height={height}
+          src="https://www.youtube.com/embed/dOFsCX72jyU"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+        <iframe
+          title={uniqueTitle}
+          width={width}
+          height={height}
+          scrolling="no"
+          frameborder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/204382042&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        />
+        <iframe
+          title={uniqueTitle}
+          src="https://open.spotify.com/embed/user/1252712964/playlist/1su6mhUPJxkVvwhmBAwpPj"
+          width={width}
+          height={height}
+          frameborder="0"
+          allowtransparency="true"
+          allow="encrypted-media"
+        />
+        <iframe
+          title={uniqueTitle}
+          src="https://open.spotify.com/embed/user/1252712964/playlist/0XVxReRFFUe7Z5DFYPdOSU"
+          width={width}
+          height={height}
+          frameborder="0"
+          allowtransparency="true"
+          allow="encrypted-media"
+        />
+        <iframe
+          title={uniqueTitle}
+          src="https://open.spotify.com/embed/user/1252712964/playlist/37i9dQZF1DWTTHy0vndijk"
+          width={width}
+          height={height}
+          frameborder="0"
+          allowtransparency="true"
+          allow="encrypted-media"
+        />
+      </div>
+    );
+  }
+}
