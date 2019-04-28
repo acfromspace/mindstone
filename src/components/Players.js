312fac3c46b5cfeb3d9b3d4699ca5ca632aad242
AC De Leon <aadeleon@sdsu.edu>
2019-04-27 20:15:19 -0700

    [*] changed soundcloud

--- src/components/Players.js
+++ src/components/Players.js
@@ -41,7 +41,7 @@ export default class Players extends Component {
           scrolling="no"
           frameborder="no"
           allow="autoplay"
-          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/204382042&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/735923574&color=%232d2d2d&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
         />
         <iframe
           title={uniqueTitle}
